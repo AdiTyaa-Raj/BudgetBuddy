0b1cf380367aef57de7bce184b2d9a2312dee378
AdiTyaa-Raj <adi7yaraj@gmail.com>
2023-08-08 16:37:41 +0530

    Code upload

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div style={{ backgroundColor: "#3F2E3E" }} className="text-light p-4">
+      <h6 className="text-center text-white">
+        Made by Aditya Raj with ❤️
+      </h6>
+    </div>
+  );
+};
+
+export default Footer;
